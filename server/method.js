a3b0376c87c15a525ae21986ebc7eb9691b61f89
gerrianna <gerri@brandeis.edu>
2016-06-28 18:51:30 -0400

    tried to make link work

--- server/method.js
+++ server/method.js
@@ -1,10 +1,12 @@
 Meteor.methods({
 	"getRecipe":
 	function(ingr,name){
+		console.dir("name =" + name);
     const z = Meteor.http.call("get",
-      "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3");
+      "http://www.recipepuppy.com/api/?q={{ingr}}");
 
-      console.dir(z);
-      return z.content;
+    console.dir(z);
+
+    return z.content;
   },
 })
