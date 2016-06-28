530b6686840567da05cda63ad5ab25c69e9656f0
demzer13 <demzer@brandeis.edu>
2016-06-28 18:33:27 -0400

    debugging method.js

--- server/method.js
+++ server/method.js
@@ -1,9 +1,9 @@
 Meteor.methods({
 	"getRecipe":
 	function(ingr,name){
+		console.dir(ingr);
     const z = Meteor.http.call("get",
       "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3");
-
       console.dir(z);
       return z.content;
   },
