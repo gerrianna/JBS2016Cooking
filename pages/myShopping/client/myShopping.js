092178f119a13fcfaa9a1a56434152902c8da0ad
gerrianna <gerri@brandeis.edu>
2016-07-18 15:42:53 -0400

    changed shopping list so it shows only the user's list

--- pages/myShopping/client/myShopping.js
+++ pages/myShopping/client/myShopping.js
@@ -1,14 +1,19 @@
 Template.myShopping.helpers({
-	shopping:function(){return Shopping.find()}
+	shopping:function(){return Shopping.find({user:Meteor.userId()})}
 })
 
 Template.myShopping.events({
 	"click .js-addShoppingItem": function(event){
 		var item = $(" .js-shoppingItem").val();
 		var shopping_obj={
-			text:item
-		}
+			text:item,
+			user:Meteor.userId()
+		};
 		console.dir(item);
 		Meteor.call("addShopping",shopping_obj);
+	},
+	"click .js-delete-shoppingItem": function(event){
+		const item = Shopping.findOne({_id:this.shopping._id});
+		Meteor.call("removeShopping",item);
 	}
 })
