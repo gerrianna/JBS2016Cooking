ff13a45c035e449564226495cabde6739fff5cb5
gerrianna <gerri@brandeis.edu>
2016-07-19 12:05:30 -0400

    tried changing url link didn't work

--- server/method.js
+++ server/method.js
@@ -1,13 +1,14 @@
 Meteor.methods({
   "getRecipe":
   function(dish){
-
     //console.dir("ingr2 = " + ingr);
     console.dir("dish2 = " + dish);
+    var apikey = Meteor.settings.spoonacular;
     //const url = "http://www.recipepuppy.com/api/?i="+ingr+"&q="+dish+"&p=3";
-    const url = "http://www.recipepuppy.com/api/?q="+dish+"&p=3";
+    const url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1";
+    //const url = "http://www.recipepuppy.com/api/?q="+dish+"&p=3";
       console.log(url);
-      const z = Meteor.http.call("get",url);
+      const z = Meteor.https.call("get",apikey,url);
       return z.content;
 
     /*console.dir("name =" + name);
