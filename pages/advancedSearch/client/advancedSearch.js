2beebb891bb704f744b12ff9b22fd96019308ca9
Demi Zhang <demizhang@Demis-MacBook-Air.local>
2016-07-19 15:32:29 -0400

    added more filters

--- pages/advancedSearch/client/advancedSearch.js
+++ pages/advancedSearch/client/advancedSearch.js
@@ -3,4 +3,24 @@ Template.advancedSearch.helpers({
     const instance = Template.instance();
     return instance.state.get("recipes");
   },
+
+  //$('#healthycheckbox').change(function(){
+  //  $('#healthydiv').toggle();
+//  });
+
+});
+
+Template.home.events({
+  "click .js-get": function(event,instance){
+    const ingr = $(".js-ingr").val();
+    const cuisine = $(".js-cuisine").val();
+    const allergies = $(".js-allergies").val();
+
+    const maxCal = $(".js-maxCal").val();
+    const maxCarb = $(".js-maxCarb").val();
+    const maxFat = $(".js-maxFat").val();
+    const maxProtein = $(".js-maxProtein").val();
+
+
+  }
 });
--- server/method.js
+++ server/method.js
@@ -1,15 +1,30 @@
 Meteor.methods({
   "getRecipe":
   function(dish){
-
     //console.dir("ingr2 = " + ingr);
     console.dir("dish2 = " + dish);
+    var apikey = Meteor.settings.spoonacular;
     //const url = "http://www.recipepuppy.com/api/?i="+ingr+"&q="+dish+"&p=3";
-    const url = "http://www.recipepuppy.com/api/?q="+dish+"&p=3";
+    const url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients="+dish+"&limitLicense=false&number=10&ranking=1";
+    //const url = "http://www.recipepuppy.com/api/?q="+dish+"&p=3";
       console.log(url);
-      const z = Meteor.http.call("get",url);
-      return z.content;
-
+     const z = HTTP.call("GET",
+      url,
+     {headers: {
+    "X-Mashape-Key": apikey,
+    "Accept": "application/json"
+      }}/*, function(error, result) {
+        if(!error) {
+          console.log("successful");
+          console.log("result");
+          console.dir(result.content);
+          console.dir("end");
+          return result.content;
+        }
+      } */
+      );
+     console.dir(z);
+     return z.content;
     /*console.dir("name =" + name);
     const z = Meteor.http.call("get",
       "http://www.recipepuppy.com/api/?q={{ingr}}");
