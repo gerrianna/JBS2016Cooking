98d0e323b37bf6f67c599cfb59268d87ff5fb113
gerrianna <gerri@brandeis.edu>
2016-07-21 10:24:50 -0400

    trying to make api.ai work with speech

--- server/method.js
+++ server/method.js
@@ -1,16 +1,13 @@
 Meteor.methods({
   "getRecipe":
   function(dish){
-    //console.dir("ingr2 = " + ingr);
     console.dir("dish2 = " + dish);
     var apikey = Meteor.settings.spoonacular;
-    //const url = "http://www.recipepuppy.com/api/?i="+ingr+"&q="+dish+"&p=3";
     const url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients="+dish+"&limitLicense=false&number=10&ranking=1";
-    //const url = "http://www.recipepuppy.com/api/?q="+dish+"&p=3";
-      console.log(url);
-     const z = HTTP.call("GET", 
-      url,
-     {headers: {
+    console.log(url);
+    const z = HTTP.call("GET", 
+    url,
+    {headers: {
     "X-Mashape-Key": apikey,
     "Accept": "application/json"
       }}/*, function(error, result) {
@@ -25,13 +22,30 @@ Meteor.methods({
       );
      console.dir(z);
      return z.content;
-    /*console.dir("name =" + name);
-    const z = Meteor.http.call("get",
-      "http://www.recipepuppy.com/api/?q={{ingr}}");
-
-    console.dir(z);
-
-    return z.content;*/
-
   },
+  "addShopping":function(shopping_obj,item){
+    console.dir(item);
+    console.log("clicked the button");
+    var apiKey = Meteor.settings.apiSpeechKey;
+    //const url = "https://api.api.ai/v1/query?v=20150910&query="+item+"&lang=en&contexts=shoppingList&sessionId="+Meteor.userId();
+    //const url = "https://api.api.ai/v1/intents/?v=20150910"
+    const url = "https://api.api.ai/v1/query?v=20150910";
+    console.log(url);
+    const z = HTTP.call("GET",
+      url,
+      {
+        headers:{
+          "Authorization": "Bearer" + apiKey,
+          "Content-type": "application/json"
+        }
+      });
+    console.dir(z);
+    Shopping.insert(shopping_obj);
+    return z;
+  },
+  "removeShopping":function(item){
+    console.dir(item);
+    console.log("clicked the x");
+    Shopping.remove(item);
+  }
 })
