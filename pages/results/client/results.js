a2f986e47da3f9c95415889707d6c7637cce2f32
demzer13 <demizhang@Demis-MacBook-Air.local>
2016-07-27 18:12:35 -0400

    results glyphicon

--- pages/results/client/results.js
+++ pages/results/client/results.js
@@ -28,35 +28,6 @@ Template.results.helpers({
 });
 
 Template.results.events({
-  "click .js-show": function(event,instance){
-    //const ingr = $(".js-ingr").val(); //this gets what the user typed in
-    const dish = $(".js-dish").val(); //this gets the dish the user want to make
-   // console.log("ingr = " + ingr);
-    console.log("dish = " + dish);
-    const number = $(".js-show").val();
-    Session.set("number",number);
-    console.log("num:")
-    console.log(number);
-    //const number = Session.get("number");
-    //Meteor.call("test1",function(e,r){console.log(r)});
-    // Meteor.call("getRecipe",[text]);
-    Meteor.apply("getRecipe",[dish,number],{returnStubValue: true},
-        function(error,result){
-          if(error) {
-            console.dir(error);
-          }
-          console.dir("result=");
-          console.dir(result);
-          r = JSON.parse(result);
-          console.dir("r= ");
-          console.dir(r);
-          x = r.results;
-          console.dir(x);
-          return Session.set("recipes",x);
-
-        }
-    );
-  },
   "click .js-talk": function(event,instance){
     console.log("clicked it");
     $(".js-talk").html("Listening...");
