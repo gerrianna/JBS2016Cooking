28f2c3e3759794901a02019ee60b5d6fc224d3c1
gerrianna <gerri@brandeis.edu>
2016-08-02 13:54:33 -0400

    added ability to add a recipe to favorites with speech

--- pages/instructions/client/instructions.js
+++ pages/instructions/client/instructions.js
@@ -2,7 +2,7 @@ Template.instructions.onCreated(function() {
   //this.state = new ReactiveDict();
   Session.setDefault({
     stepNum:0,
-    ingredients: []
+    ingredients: [],
     //offset:90,
   });
 });
@@ -19,11 +19,21 @@ Template.instructions.helpers({
     var ingredients = [];
     console.log(ingr.extendedIngredients.length);
     console.log(ingr.extendedIngredients[1].originalString.length);
+    var recipeName = ingr.title;
+    var recipeId = ingr.id;
+    var recipeImage = ingr.image;
+    console.log(ingr.title);
+    console.log(ingr.id);
     for(var i=0; i<ingr.extendedIngredients.length; i++){
         console.log(ingr.extendedIngredients[i].originalString);
         ingredients.push(ingr.extendedIngredients[i].originalString);
     }
     Session.set("ingredients",ingredients);
+    Session.set("recipeName",recipeName);
+    Session.set("recipeId",recipeId);
+    Session.set("recipeImage",recipeImage);
+
+
     var newone = _.uniq(ingredients);
 
     function register(){
